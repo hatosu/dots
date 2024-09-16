53db2593dfeea5b98049009e8887d59d567fd837
Hatosu <tylerjacobcox@tutamail.com>
2024-09-15 22:55:21 -0700

    Delete etc/nixos directory

--- etc/nixos/profiles/laptop/modules/hodules/homelinks.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, inputs, ... }: {
-
-  #create symlinks for programs that need it, with folders & files in resources
-  home.file = {
-    ".config/river/" = { source = ../../resources/river; target = ".config/river/"; };
-  };
-
-}
