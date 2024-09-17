9f4b4a0069244e0767fd5a9634491cf226a1a0da
Hatosu <tylerjacobcox@tutamail.com>
2024-09-17 09:39:43 -0700

    Create configuration.nix

---
+++ NixOS/profiles/temp/configuration.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  
+  imports = 
+    [
+      ./hardware-configuration.nix
+      ./modules/codules/core.nix
+      ./modules/codules/driver.nix
+      ./modules/codules/font.nix
+      ./modules/codules/game.nix
+      ./modules/codules/hotkey.nix
+      ./modules/codules/manage.nix
+      ./modules/codules/package.nix
+      ./modules/codules/secure.nix
+      ./modules/codules/variable.nix
+      ./modules/codules/virtualization.nix
+      ./scripts/rebuild.nix
+      ./scripts/startup.nix
+      ./scripts/functions.nix
+      ./scripts/alias.nix
+    ];
+
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration
+  system.stateVersion = "24.05"; # Did you read the comment?
+  
+}
